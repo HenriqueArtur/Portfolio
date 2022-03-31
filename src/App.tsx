7bff3deb8bf7694d6c8319084aa0813d359b3998
HenriqueArtur <henriqueacg@gmail.com>
2022-03-30 22:59:46 -0300

    :sparkles: - add typewrite

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { BrowserView, MobileView } from 'react-device-detect';
 import { Container, Row, Col } from 'react-bootstrap';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faLinkedinIn, faGithubAlt, faTelegramPlane } from "@fortawesome/free-brands-svg-icons"
-import { faCircle } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedinIn, faGithubAlt, faTelegramPlane } from "@fortawesome/free-brands-svg-icons";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+import Typewriter from 'typewriter-effect';
 import './styles/provisory.scss';
 
 const links = {
@@ -26,19 +28,73 @@ const App: React.FC = () => {
 
   return (
     <>
+      <MobileView>
+        <Container>
+          <Row>
+            <Col sm={12} md={8} lg={6}>
+              <div className="circles mobile-circles">
+                <FontAwesomeIcon icon={faCircle} size='sm' color={colors.red} className='circle' />
+                <FontAwesomeIcon icon={faCircle} size='sm' color={colors.yellow} className='circle' />
+                <FontAwesomeIcon icon={faCircle} size='sm' color={colors.green} className='circle' />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </MobileView>
       <Container>
         <Row>
           <Col>
             <div className="all_height">
               <div className="vertical_align">
-                <div className="circles">
-                  <FontAwesomeIcon icon={faCircle} size='sm' color={colors.red} className='circle' />
-                  <FontAwesomeIcon icon={faCircle} size='sm' color={colors.yellow} className='circle' />
-                  <FontAwesomeIcon icon={faCircle} size='sm' color={colors.green} className='circle' />
-                </div>
-                <h1>Henrique Artur</h1>
-                <h3>Full-Stack Developer</h3>
-                <span>Creativity, Design and Development</span>
+                <Container>
+                  <BrowserView>
+                    <Row>
+                      <Col sm={{ span: 12, offset: 0 }} md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
+                        <div className="circles">
+                          <FontAwesomeIcon icon={faCircle} size='sm' color={colors.red} className='circle' />
+                          <FontAwesomeIcon icon={faCircle} size='sm' color={colors.yellow} className='circle' />
+                          <FontAwesomeIcon icon={faCircle} size='sm' color={colors.green} className='circle' />
+                        </div>
+                      </Col>
+                    </Row>
+                  </BrowserView>
+                </Container>
+                <h1>
+                  <Typewriter
+                    onInit={(typewriter) => {
+                      typewriter
+                        .typeString('Henrique Artur')
+                        .callFunction(function (state) {
+                          state.elements.cursor.style.display = 'none';
+                        })
+                        .start()
+                    }}
+                    options={{ cursorClassName: 'cursor', delay: 100 }} />
+                </h1>
+                <h3>
+                  <Typewriter
+                    onInit={(typewriter) => {
+                      typewriter
+                        .typeString('Full-Stack Developer')
+                        .callFunction(function (state) {
+                          state.elements.cursor.style.display = 'none';
+                        })
+                        .start()
+                    }}
+                    options={{ cursorClassName: 'cursor', delay: 55 }} />
+                </h3>
+                <span>
+                  <Typewriter
+                    onInit={(typewriter) => {
+                      typewriter
+                        .typeString('Creativity, Design and Development')
+                        .callFunction(function (state) {
+                          state.elements.cursor.style.display = 'none';
+                        })
+                        .start()
+                    }}
+                    options={{ cursorClassName: 'cursor', delay: 35 }} />
+                </span>
                 <div className="contact">
                   <div className="icons">
                     <a href={links.linkedIn} target="_blank" rel="noreferrer">
